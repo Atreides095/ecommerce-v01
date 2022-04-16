37b9431a831475bb66bbbeeb03c3eb4dab4ec518
Erick Aguilar <eaguilar_95@outlook.com>
2022-04-16 05:15:38 -0600

    first commit

---
+++ pages/login.js
@@ -0,0 +1,44 @@
+import {
+    Button,
+    Flex,
+    Heading,
+    Input,
+    useColorMode,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+const Login = () => {
+    const { toggleColorMode } = useColorMode();
+    const formBackground = useColorModeValue('gray.100', 'gray.700');
+
+    return (
+        <Flex height={'100vh'} alignItems={'center'} justifyContent={'center'}>
+            <Flex
+                direction="column"
+                background={formBackground}
+                p={12}
+                rounded={6}
+            >
+                <Heading mb={6}>Sign in</Heading>
+                <Input
+                    placeholder="email@provider.com"
+                    variant="filled"
+                    mb={3}
+                    type="email"
+                />
+                <Input
+                    placeholder="*******"
+                    variant="filled"
+                    mb={6}
+                    type="password"
+                />
+                <Button mb={6} colorScheme="teal">
+                    Sign in
+                </Button>
+                <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
+            </Flex>
+        </Flex>
+    );
+};
+
+export default Login;
---
+++ styles/theme.js
@@ -0,0 +1,13 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+    fonts: {
+        body: 'Inter, sans-serif',
+        heading: 'Inter, sans-serif',
+    },
+    styles: {
+        global: {},
+    },
+});
+
+export default theme;
