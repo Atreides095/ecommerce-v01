f7bc370acd5291898b5bde6d0f13354cae07ff1a
Erick Aguilar <eaguilar_95@outlook.com>
2022-04-18 07:07:23 -0600

    fixed some issues with the data

--- data/cleaner.js
+++ data/cleaner.js
@@ -10,44 +10,72 @@ const files = [
     'gamingPc',
 ];
 
-for (let file of files) {
-    const data = require(`./${file}.json`);
-    const newData = data.map((item) => {
-        item.image_single = item.image_single_big.replace(
-            'https://img.computerunivers.net/images/1000/',
-            '/'
-        );
+// for (let file of files) {
+//     const data = require(`./old/${file}.json`);
+//     const newData = data.map((item) => {
+//         item.image_single = item.image_single_big.replace(
+//             'https://img.computerunivers.net/images/1000/',
+//             '/'
+//         );
 
-        item.image_list = item.image_list_big.map((item) => {
-            return item.replace(
-                'https://img.computerunivers.net/images/1000/',
-                '/'
-            );
-        });
+//         item.image_list = item.image_list_big.map((item) => {
+//             return item.replace(
+//                 'https://img.computerunivers.net/images/1000/',
+//                 '/'
+//             );
+//         });
 
-        item.details = item.facets;
+//         item.details = item.facets;
 
-        delete item.image_single_square;
-        delete item.image_list_square;
-        delete item.image_single_big;
-        delete item.image_list_big;
-        delete item.facets;
-        delete item.category_single;
+//         delete item.image_single_square;
+//         delete item.image_list_square;
+//         delete item.image_single_big;
+//         delete item.image_list_big;
+//         delete item.facets;
 
-        return item;
-    });
+//         return item;
+//     });
 
-    fs.writeFile(
-        `./data/${file}Clean.json`,
-        JSON.stringify(newData),
-        'utf8',
-        (err) => {
-            if (err) {
-                console.log(err);
-                return console.log(err);
-            }
+//     fs.writeFile(
+//         `./data/${file}Clean.json`,
+//         JSON.stringify(newData),
+//         'utf8',
+//         (err) => {
+//             if (err) {
+//                 console.log(err);
+//                 return console.log(err);
+//             }
 
-            console.log('JSON file has been saved');
-        }
-    );
-}
+//             console.log('JSON file has been saved');
+//         }
+//     );
+// }
+
+// const data = require('./old/tvVideo.json');
+
+// const dataNew = data.results[0].hits.map((item, i) => {
+//     return {
+//         name: item.name,
+//         image_single: `${item.image_url}`.replace('/images/250x250', ''),
+//         image_list: item.image_url_set.map((item) =>
+//             `${item}`.replace('/images/250x250', '')
+//         ),
+//         details: item.facets,
+//         manufacturer: item.manufacturer,
+//         price: Math.floor(item.price_ag_floored * 1.08),
+//         category_single: item.categoryname,
+//         category_list: item.categorynames,
+//         timestamp_created: item.timestamp_created,
+//         bulletpoints: item.bulletpoints,
+//         popularity: item.popularity_modified,
+//     };
+// });
+
+// fs.writeFile('./data/tvVideo.json', JSON.stringify(dataNew), 'utf8', (err) => {
+//     if (err) {
+//         console.log(err);
+//         return console.log(err);
+//     }
+
+//     console.log('JSON file has been saved');
+// });
